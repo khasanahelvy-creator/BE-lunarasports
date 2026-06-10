1c7614b89046f3b7f6e9881114185baa03cb5cc5
Elvy <khasanahelvy@gmail.com>
2026-06-10 10:30:00 +0800

    feat: registrasi layanan appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,25 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+// Tambahkan import konfigurasi Midtrans
+use Midtrans\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Pengaturan Midtrans
+        Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        // Set ke Development/Sandbox Environment (default false)
+        Config::$isProduction = env('MIDTRANS_IS_PRODUCTION', false);
+        // Set sanitization (bersihkan input data)
+        Config::$isSanitized = true;
+        // Set 3DS transaksi untuk credit card
+        Config::$is3ds = true;
     }
 }
