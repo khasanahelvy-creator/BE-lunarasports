338014c564512425f3bdc0a33d3c6436ac6de2ab
Elvy <khasanahelvy@gmail.com>
2026-06-23 15:37:30 +0800

    feat(voucher): add vouchers table

---
+++ database/migrations/2026_06_23_073026_create_vouchers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->enum('discount_type', ['percent', 'fixed'])->default('fixed');
+            $table->integer('discount_value');
+            $table->integer('max_discount')->nullable(); // Maksimal potongan untuk tipe percent
+            $table->integer('min_transaction')->default(0);
+            $table->timestamp('expires_at')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('vouchers');
+    }
+};
